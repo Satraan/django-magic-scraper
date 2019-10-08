0ab0ee451f3de87e8a55215732de00112cc415a8
Mandia <mandiasamuels@gmail.com>
2019-10-08 09:19:35 +0200

    Implemented Downshift

--- frontend/src/components/Searchbar.js
+++ frontend/src/components/Searchbar.js
@@ -11,20 +11,68 @@ import {
   Input,
   Label
 } from "reactstrap"
+import Downshift from "downshift"
 
-const StyledForm = styled(Form)`
+const StyledSelect = styled.div`
   max-width: 50%;
   text-align: center;
   margin: auto;
 `
+const items = [
+  { value: "apple" },
+  { value: "pear" },
+  { value: "orange" },
+  { value: "grape" },
+  { value: "banana" }
+]
 
 const Searchbar = () => {
   return (
-    <StyledForm>
-      <FormGroup>
-        <Input type="text" name="card" id="cardSearch" placeholder="Search for a card..." />
-      </FormGroup>
-    </StyledForm>
+    <Downshift
+      onChange={selection =>
+        alert(selection ? `You selected ${selection.value}` : "Selection Cleared")
+      }
+      itemToString={item => (item ? item.value : "")}
+    >
+      {({
+        getInputProps,
+        getItemProps,
+        getLabelProps,
+        getMenuProps,
+        isOpen,
+        inputValue,
+        highlightedIndex,
+        selectedItem
+      }) => (
+        <div>
+          <StyledSelect>
+            <label {...getLabelProps()}>Enter a fruit</label>
+            <input {...getInputProps()} />
+            <ul {...getMenuProps()}>
+              {isOpen
+                ? items
+                    .filter(item => !inputValue || item.value.includes(inputValue))
+                    .map((item, index) => (
+                      <li
+                        {...getItemProps({
+                          key: item.value,
+                          index,
+                          item,
+                          style: {
+                            backgroundColor: highlightedIndex === index ? "lightgray" : "white",
+                            fontWeight: selectedItem === item ? "bold" : "normal"
+                          }
+                        })}
+                      >
+                        {item.value}
+                      </li>
+                    ))
+                : null}
+            </ul>
+          </StyledSelect>
+        </div>
+      )}
+    </Downshift>
   )
 }
 
